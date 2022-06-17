8c3adc076673c9af79cf8d4988627d37019fb5d5
Jun Aruga <jaruga@redhat.com>
2022-06-17 22:41:05 +0200

    Fix run path test by removing the `--prefix` option in the `./configure`.
    I find the option causes the failure of the run path test.
    Updated the ruby binaries too.

--- build_with_fedora_build_flags.sh
+++ build_with_fedora_build_flags.sh
@@ -9,7 +9,6 @@ CFLAGS='-O2 -flto=auto -ffat-lto-objects -fexceptions -g -grecord-gcc-switches -
 CXXFLAGS='-O2 -flto=auto -ffat-lto-objects -fexceptions -g -grecord-gcc-switches -pipe -Wall -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -Wp,-D_GLIBCXX_ASSERTIONS -specs=/usr/lib/rpm/redhat/redhat-hardened-cc1 -fstack-protector-strong -specs=/usr/lib/rpm/redhat/redhat-annobin-cc1  -m64  -mtune=generic -fasynchronous-unwind-tables -fstack-clash-protection -fcf-protection' \
 LDFLAGS='-Wl,-z,relro -Wl,--as-needed  -Wl,-z,now -specs=/usr/lib/rpm/redhat/redhat-hardened-ld -specs=/usr/lib/rpm/redhat/redhat-annobin-cc1  -Wl,--build-id=sha1 ' \
 ./configure \
-    --prefix="$(pwd)/dest" \
     --enable-shared \
     --enable-mkmf-verbose 2>&1 | tee configure.log
 
@@ -17,4 +16,3 @@ make V=1 2>&1 | tee make.log
 
 # annocheck ./ruby
 # make test-annocheck
-# make install V=1 2>&1 | tee make_install.log
--- build_with_minimal_flags.sh
+++ build_with_minimal_flags.sh
@@ -8,7 +8,6 @@ set -eux
 # https://bugs.ruby-lang.org/issues/18061#note-22
 LDFLAGS=-Wl,-z,now \
 ./configure \
-    --prefix="$(pwd)/dest" \
     --enable-shared \
     --with-gcc="gcc -fcf-protection" \
     --enable-mkmf-verbose 2>&1 | tee configure.log
